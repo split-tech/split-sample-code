71c954f5c9fd77c443229c3bb6042bc279dbf954
miju3545 <miju3545@gmail.com>
2024-05-29 14:04:45 +0900

    Chore: Netlify Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportTrailingSlash: true,
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
