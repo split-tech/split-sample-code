1f6d234072d71145961d038deb32f2b5aca2e1fa
miju3545 <miju3545@gmail.com>
2024-05-29 14:21:06 +0900

    Feat: SplitBrowerProvider 위치 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SplitBrowserProvider } from '@split-tech/browser-sdk';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <SplitBrowserProvider apiKey="" config={{}}>
-      <Component {...pageProps} />
-    </SplitBrowserProvider>
-  );
+  return <Component {...pageProps} />;
 }
